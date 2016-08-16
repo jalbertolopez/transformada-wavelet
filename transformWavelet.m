0e96e8ccf571cec35f7c8fb2a16c46be6e1ff033
Alberto Lopez <jalbertolopezflores8@gmail.com>
2016-08-15 23:52:49 -0500

    Convoluciones de la matriz de la imagen con los filtros

--- transformWavelet.m
+++ transformWavelet.m
@@ -14,3 +14,159 @@ longuitudFiltroA = length( filtroPasoAlto );
 
 convolucionesB = zeros( longuitudFiltroB );
 pasoBajo = zeros( longuitudFiltroB );
+
+for filaExterior = 1 : m
+    for fila = 1 : longuitudFiltroB 
+        for filaInterior = 1 : n
+            
+            convolucionesB( fila, ( fila - 1 ) + filaInterior ) = ...
+                filtroPasoBajo( fila ) * imagenRecibida( filaExterior, ...
+                filaInterior ); 
+        end
+    end
+    sumaResultadosB = sum( convolucionesB );
+    dimension = length( sumaResultadosB );
+    
+    j = 1;
+    
+    for i = 2 : 2 : dimension
+        pasoBajo( filaExterior, j  ) = sumaResultadosB( i );
+        j = j + 1;
+    end
+end
+
+
+[ m, n ] = size( pasoBajo );
+
+convolucionesBB = zeros( longuitudFiltroB );
+pasoBajoBajo = zeros( longuitudFiltroB );
+
+for columnaExterior = 1 : n
+    for fila = 1 : longuitudFiltroB 
+        for columnaInterior = 1 : m
+            
+            convolucionesBB( fila, ( fila - 1 ) + columnaInterior ) = ...
+                filtroPasoBajo( fila ) * pasoBajo( columnaInterior, ...
+                columnaExterior ); 
+        end
+    end
+    sumaResultados = sum( convolucionesBB );
+    dimension = length( sumaResultados );
+    
+    j = 1;
+    
+    for i = 2 : 2 : dimension
+        pasoBajoBajo( j, columnaExterior  ) = sumaResultados( i );
+        j = j + 1;
+    end
+end
+
+pBB = pasoBajoBajo;
+
+[ m, n ] = size( pasoBajo );
+
+convolucionesBA = zeros( longuitudFiltroA );
+pasoBajoAlto = zeros( longuitudFiltroA );
+
+for columnaExterior = 1 : n
+    for fila = 1 : longuitudFiltroA 
+        for columnaInterior = 1 : m
+            
+            convolucionesBA( fila, ( fila - 1 ) + columnaInterior ) = ...
+                filtroPasoAlto( fila ) * pasoBajo( columnaInterior, ...
+                columnaExterior ); 
+        end
+    end
+    sumaResultados = sum( convolucionesBA );
+    dimension = length( sumaResultados );
+    
+    j = 1;
+    
+    for i = 2 : 2 : dimension
+        pasoBajoAlto( j, columnaExterior  ) = sumaResultados( i );
+        j = j + 1;
+    end
+end
+
+pBA = pasoBajoAlto;
+
+[ m, n ] = size( imagenRecibida );
+
+matrizConvolucionesA = zeros( longuitudFiltroB );
+pasoAlto = zeros( longuitudFiltroB );
+
+for filaExterior = 1 : m
+    for fila = 1 : longuitudFiltroB 
+        for filaInterior = 1 : n
+            
+            matrizConvolucionesA( fila, ( fila - 1 ) + filaInterior ) = ...
+                filtroPasoAlto( fila ) * imagenRecibida( filaExterior, ...
+                filaInterior ); 
+        end
+    end
+    matrizResultadosA = sum( matrizConvolucionesA );
+    dimension = length( matrizResultadosA );
+    
+    j = 1;
+    
+    for i = 2 : 2 : dimension
+        pasoAlto( filaExterior, j  ) = matrizResultadosA( i );
+        j = j + 1;
+    end
+end
+
+[ m, n ] = size( pasoAlto );
+
+convolucionesAB = zeros( longuitudFiltroA );
+pasoAltoBajo = zeros( longuitudFiltroA );
+
+for columnaExterior = 1 : n
+    for fila = 1 : longuitudFiltroB 
+        for columnaInterior = 1 : m
+            
+            convolucionesAB( fila, ( fila - 1 ) + columnaInterior ) = ...
+                filtroPasoBajo( fila ) * pasoAlto( columnaInterior, ...
+                columnaExterior ); 
+        end
+    end
+    sumaResultados = sum( convolucionesAB );
+    dimension = length( sumaResultados );
+    
+    j = 1;
+    
+    for i = 2 : 2 : dimension
+        pasoAltoBajo( j, columnaExterior  ) = sumaResultados( i );
+        j = j + 1;
+    end
+end
+
+pAB =  pasoAltoBajo ;
+
+[ m, n ] = size( pasoAlto );
+
+convolucionesAA = zeros( longuitudFiltroA );
+pasoAltoAlto = zeros( longuitudFiltroA );
+
+for columnaExterior = 1 : n
+    for fila = 1 : longuitudFiltroA 
+        for columnaInterior = 1 : m
+            
+            convolucionesAA( fila, ( fila - 1 ) + columnaInterior ) = ...
+                filtroPasoAlto( fila ) * pasoAlto( columnaInterior, ...
+                columnaExterior ); 
+        end
+    end
+    sumaResultados = sum( convolucionesAA );
+    dimension = length( sumaResultados );
+    
+    j = 1;
+    
+    for i = 2 : 2 : dimension
+        pasoAltoAlto( j, columnaExterior  ) = sumaResultados( i );
+        j = j + 1;
+    end
+end
+
+pAA = pasoAltoAlto;
+
+end
